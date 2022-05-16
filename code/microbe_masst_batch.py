7f00a5e8b6810b1a1c36476aef4192d30ba82c15
robinschmid <robinschmid@uni-muenster.de>
2022-05-16 17:46:32 +0200

    Export masst results, clean up

--- code/microbe_masst_batch.py
+++ code/microbe_masst_batch.py
@@ -6,7 +6,7 @@ from urllib import parse
 from tqdm import tqdm
 import re
 
-import microbe_masst as micromasst
+import microbe_masst as microbemasst
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -24,7 +24,7 @@ def get_html_link(file_name, compound_name):
 def run_job(file_name, usi_or_lib_id, compound_name):
     out_html = "../{}_{}.html".format(file_name, compound_name.replace(" ", "_"))
 
-    result = micromasst.run_microbe_masst(usi_or_lib_id, precursor_mz_tol=0.05, mz_tol=0.02, min_cos=0.7,
+    result = microbemasst.run_microbe_masst(usi_or_lib_id, precursor_mz_tol=0.05, mz_tol=0.02, min_cos=0.7,
                                           in_html="collapsible_tree_v3.html",
                                           in_ontology="../data/ncbi.json",
                                           metadata_file="../data/microbe_masst_table.csv",
@@ -36,52 +36,19 @@ def run_job(file_name, usi_or_lib_id, compound_name):
     return result
 
 
-# jobs = {
-#         "CCMSLIB00005883671": "GABA",
-#         "CCMSLIB00006582001": "phe_CA",
-#         "CCMSLIB00006581985": "glu_CA",
-#         "CCMSLIB00000006885": "surfactin_C13",
-#         "CCMSLIB00000006895": "surfactin_C15",
-#         "CCMSLIB00005721043": "salinosporamide_A",
-#         "CCMSLIB00006694017": "salinomycin",
-#         "CCMSLIB00000006871": "actinomycin_d",
-#         "CCMSLIB00003134958": "actinomycin_d_nist14_match",
-#         "CCMSLIB00000075066": "arylomycin_a4",
-#         "CCMSLIB00005772087": "kanamycin_a",
-#         "CCMSLIB00005464333": "PANTOTHENATE",
-#         "CCMSLIB00005723628": "acyl_ferrioxamine_2",
-#         "CCMSLIB00000072229": "PQS",
-#         "CCMSLIB00000072137": "NHQ",
-#         "CCMSLIB00000072038": "putative quinolone",
-#         "CCMSLIB00003136275": "lovastatin",
-#         "CCMSLIB00005435739": "lovastatin_2",
-#         "CCMSLIB00004679270": "peptaibol",
-#         "CCMSLIB00003134635": "azithromycin",
-#         "CCMSLIB00000579271": "Surugamide A",
-#         "CCMSLIB00000001621": "Desferrioxamine E",
-#         "CCMSLIB00000072100": "Desferrioxamine B",
-#         "CCMSLIB00005716848": "Acyl_Desferrioxamine_C13_Promicroferrioxamine",
-#         "CCMSLIB00003739952": "Penicillin G",
-#         "CCMSLIB00005435755": "Ferrichrome",
-#         "CCMSLIB00000070253": "Barbamide",
-#         "CCMSLIB00000001562": "Jamaicamide A",
-#         "CCMSLIB00000075016": "Napsamycin",
-#         "CCMSLIB00001059079": "triacylfusarin",
-#         "CCMSLIB00000074975": "Fusarin A",
-#         "CCMSLIB00004721498": "Polymyxin B",
-#         "CCMSLIB00005732667": "Microcystin LR",
-#         "CCMSLIB00000005120": "Mycophenolic",
-#         "CCMSLIB00005755738": "Mycotoxin F2",
-#         "CCMSLIB00005723573": "Beauvericin",
-#         "CCMSLIB00005727552": "Beauvericin_2"
-#     }
-
 def path_safe(file):
     return re.sub('[^-a-zA-Z0-9_.() ]+', '_', file)
 
 
+def explode_masst_columns(jobs_df):
+    masst_df = jobs_df.explode("fastMASST", ignore_index=True)
+    masst_df = masst_df.join(pd.DataFrame([masst for masst in masst_df['fastMASST']]))
+    masst_df.drop(columns=["fastMASST"], inplace=True)
+    return masst_df
+
 if __name__ == '__main__':
-    # jobs = {k: v.replace(" ", "_") for (k, v) in jobs.items()}
+    input_file = "../examples/emily.csv"
+    # output
     file_name = "emily/fast_microbeMasst"
     finished_jobs_tsv = "../emily/finished.tsv"
     masst_results_tsv = "../{}.tsv".format(file_name)
@@ -94,24 +61,19 @@ if __name__ == '__main__':
 
     # read list of jobs
     # jobs_df = pd.read_csv("../emily/Combinatorial_reactions_USIs - fatty acid amides.tsv", sep="\t")
-    jobs_df = pd.read_csv("../examples/emily_short.csv", sep=",")
-    jobs_df.rename(columns={'USI': 'ID', 'COMPOUND_NAME': 'Compound'}, inplace=True)
+    jobs_df = pd.read_csv(input_file, sep=",")
+    jobs_df.rename(columns={'USI': 'input_id', 'COMPOUND_NAME': 'Compound'}, inplace=True)
     # jobs_df.rename(columns={'Output USI': 'ID', 'COMPOUND_NAME': 'Compound'}, inplace=True)
     jobs_df["Compound"] = jobs_df["Compound"].apply(path_safe)
 
-    jobs_df["fastMASST"] = jobs_df.progress_apply(lambda row: run_job(file_name, row["ID"], row["Compound"]), axis=1)
+    jobs_df["fastMASST"] = jobs_df.progress_apply(lambda row: run_job(file_name, row["input_id"], row["Compound"]), axis=1)
     jobs_df["Tree"] = jobs_df.progress_apply(lambda row:
                                              get_html_link(file_name, row["Compound"]) if row["fastMASST"] is not
                                                                                           None else None,
                                              axis=1)
 
     # export masst results
-    # jobs_df['fastMASST'].apply(pd.Series)
-    masst_df = jobs_df.explode("fastMASST", ignore_index=True)
-    # masst_df = masst_df.merge(masst_df['fastMASST'].apply(pd.Series))
-    masst_df = masst_df.join(pd.json_normalize(masst_df['fastMASST']))
-    masst_df.rename(columns={"ID": "input_id"}, inplace=True)
-
+    masst_df = explode_masst_columns(jobs_df)
     masst_df.to_csv(masst_results_tsv, sep="\t", index=False)
 
     # export the list of links
